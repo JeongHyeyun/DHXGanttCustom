ef7b122d888a0dc7cc82428b449f11672c5db32a
Hyeyun <yuuuuuuni@gmail.com>
2021-06-21 20:58:43 +0900

    init env

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,8 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  apiKey: '"AIzaSyC6QOaLM1bBSoI2RhbJfUb5syZ2WQDJ-E8"',
+  messagingSenderId: '"655502256372"',
+  appId: '"1:655502256372:web:33b2af1d8a494c7e7a56a1"',
+  measurementId: '"G-J9LV6QV39L"'
 }
---
+++ firebaseconfig.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: "dhxgantt.firebaseapp.com",
+    databaseURL: "https://dhxgantt-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "dhxgantt",
+    storageBucket: "dhxgantt.appspot.com",
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+  };
+
+  export default firebaseConfig;
